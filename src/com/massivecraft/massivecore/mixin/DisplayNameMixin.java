d6b11a2e272a62913c6d761442103fd79940d08e
Ryan Sjostrand <nowirelss@gmail.com>
2014-06-14 23:55:08 -0500

    Updated to Maven

--- src/com/massivecraft/massivecore/mixin/DisplayNameMixin.java
+++
@@ -1,7 +0,0 @@
-package com.massivecraft.massivecore.mixin;
-
-public interface DisplayNameMixin
-{
-	public String getDisplayName(Object senderObject);
-	public void setDisplayName(Object senderObject, String displayName);
-}
